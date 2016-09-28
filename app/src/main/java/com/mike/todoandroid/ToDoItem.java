d8e77e84dd2377187fd6c4b1ba8d09456226da72
michaelplott <mike.plott80@gmail.com>
2016-09-28 10:31:48 -0400

    Initializing arrayAdapter and starting to connect everything.

---
+++ app/src/main/java/com/mike/todoandroid/ToDoItem.java
@@ -0,0 +1,16 @@
+package com.mike.todoandroid;
+
+/**
+ * Created by michaelplott on 9/28/16.
+ */
+
+public class ToDoItem {
+    String text;
+    boolean isDone;
+
+    public ToDoItem(String text, boolean isDone) {
+        this.text = text;
+        this.isDone = isDone;
+    }
+
+}
